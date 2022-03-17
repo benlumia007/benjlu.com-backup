e33dc76055c4e132c01bff53d78a15a8fa011e5d
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-16 21:46:07 -0700

    let's add a siteTitle and siteTagline to the site

--- config.php
+++ config.php
@@ -2,6 +2,13 @@
 
 return [
     'build' => [
-        'source' => 'public'
+        // path to your content, blades, assets, etc.
+        'source' => 'public',
     ],
+
+    // a custom title for the site.
+    'siteTitle' => 'Benjamin Lu',
+
+    // a custom tagline for the site.
+    'siteTagline' => 'Frontend & Backend Developer'
 ];
