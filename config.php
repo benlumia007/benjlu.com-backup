8da4e5e103952d80fd0be0b89278f3d1b7bd0e0a
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-17 23:45:15 -0700

    updated

--- config.php
+++ config.php
@@ -24,6 +24,7 @@ return [
     'collections' => [
         'portfolios' => [
             'path' => 'portfolio/{-title}',
+            'extends' => '_types.portfolio',
             'sort' => '-date',
         ],
     ],
--- public/_layouts/home.blade.php
+++ public/_layouts/home.blade.php
@@ -1,1 +1,23 @@
 @extends( '_templates.home' )
+
+@section(  'body' )
+<section id="content" class="site-content">
+    <div class="content-area">
+        <article class="portfolio">
+            <ul class="grid">
+                @foreach ( $portfolios as $portfolio )
+                    <li>
+                        <figure class="post-thumbnail">
+                            <img src="/{{ $portfolio->thumbnail }}">
+                        </figure>
+                        <div class="caption">
+                            <h3 class="caption-title"><a href="{{$portfolio->getUrl() }}">{{ $portfolio->title }}</a></h3>
+                            <span class="caption-type">{{ ucwords( str_replace( [ 'p', '-' ], [ 'P', ' ' ], $portfolio->type ) ) }}</span>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </article>
+    </div>
+</section>
+@endsection
