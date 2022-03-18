fd0b8b8793c5f37f9a9f834f02232172d664cb67
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-17 23:54:43 -0700

    updated

--- public/_types/portfolio.blade.php
+++ public/_types/portfolio.blade.php
@@ -3,16 +3,14 @@
 @section( 'body' )
     <section id="content" class="site-content">
         <div class="content-area">
-            @foreach ( $portfolios as $post )
-                <article class="post">
-                    <header class="entry-header">
-                        <h1 class="entry-title">{{ $page->title }}</a></h1>
-                    </header>
-                    <div class="entry-content">
-                        @yield( 'content' )
-                    </div>
-                </article>
-            @endforeach
+            <article class="portfolio">
+                <header class="entry-header">
+                    <h1 class="entry-title">{{ $page->title }}</a></h1>
+                </header>
+                <div class="entry-content">
+                    @yield( 'content' )
+                </div>
+            </article>
         </div>
     </section>
 @endsection
