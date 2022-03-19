ba808066abd9d44fcfa44639a32de3e4eb37d783
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-18 22:08:55 -0700

    deleted everything to begin blush development

--- helpers.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-use Illuminate\Support\Str;
-
-return [
-    'selected' => function( $page, $selection ) {
-        return Str::contains( $page->getPath(), $selection ) ? 'active' : '';
-    },
-];
