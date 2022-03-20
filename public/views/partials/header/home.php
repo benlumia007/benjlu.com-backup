d529efea0256205366e092df151a8a1249a27460
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-20 13:20:37 -0700

    add function helper for site title and site description

--- public/views/partials/header/home.php
+++ public/views/partials/header/home.php
@@ -3,7 +3,7 @@
 <head>
     <?= $doctitle ? $doctitle->toHtml() : '' ?>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="<?= assets( 'assets/css/screen.css' ); ?>" />
+    <link rel="stylesheet" href="<?= mix( 'assets/css/screen.css' ); ?>" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700&family=Merriweather:wght@400;700&family=Tangerine&display=swap" rel="stylesheet">
@@ -11,20 +11,11 @@
 </head>
 <body>
 <section id="container" class="site-container">
-    <nav id="primary" class="menu-primary">
-        <button class="menu-toggle"> <?= e( 'Menu' ); ?></button>
-        <ul class="menu-items">
-            <?php 
-                $primaryMenu = config( 'app', 'primary' );
-                foreach ($primaryMenu as $primary ) : ?>
-                    <li class="menu-item"><a href="<?= $primary['link']; ?>"><?= $primary[ 'title' ]; ?></a></li>
-                <?php endforeach; ?>
-        </ul>
-    </nav>
+    <?= primary_menu(); ?>
     <header id="masthead" class="site-header custom-image">
         <div class="site-avatar"></div>
         <div class="site-branding">
-            <h1 class="site-title"><a href="<?= uri() ?>"><?= config( 'app', 'title' ); ?></a></h1>
-            <span class="site-description"><?= config( 'app', 'description' ); ?></span>
+            <h1 class="site-title"><a href="<?= uri() ?>"><?= site_title(); ?></a></h1>
+            <span class="site-description"><?= site_tagline(); ?></span>
         </div>
     </header>
