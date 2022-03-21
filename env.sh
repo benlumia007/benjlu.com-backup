d9d563a74973924313052d61814c44e4b291c2c0
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-21 09:56:35 -0700

    udpated

--- env.sh
+++ env.sh
@@ -4,6 +4,6 @@ if [[ ! -f "/home/benjlu/public_html/.env" ]]; then
     cp -r "/home/benjlu/actions-runner/benjlu/benjlu.com/benjlu.com/benjlu/.env.example" "/home/benjlu/public_html/.env"
 
     if grep -q "http://localhost" "/home/benjlu/public_html/.env"; then
-        sudo sed -i -e "s/http://localhost/https://benjlu.com/g" "/home/benjlu/public_html/.env"
+        sed -i -e "s/http://localhost/https://benjlu.com/g" "/home/benjlu/public_html/.env"
     fi
 fi
