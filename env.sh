3c60e024442bf67af89b67fae567db088e1910a1
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-21 10:25:25 -0700

    fixed, i hope

--- env.sh
+++ env.sh
@@ -4,6 +4,7 @@ if [[ ! -f "/home/benjlu/public_html/.env" ]]; then
     cp -r "/home/benjlu/actions-runner/benjlu/benjlu.com/benjlu.com/benjlu/.env.example" "/home/benjlu/public_html/.env"
 
     if grep -q "http://localhost" "/home/benjlu/public_html/.env"; then
-        sed -i -e "s/http://localhost/https://benjlu.com/g" "/home/benjlu/public_html/.env"
+        sed -i -e "s/http:\/\/localhost/https:\/\/benjlu.com/g" "/home/benjlu/public_html/.env"
+
     fi
 fi
