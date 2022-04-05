ebb7e2d170f00e5db33b003bfbd2bf0af13373ef
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-04-04 21:36:46 -0700

    empty repo

--- public/views/partials/footer/default.php
+++
@@ -1,9 +0,0 @@
-    <footer id="footer" class="site-footer">
-        <div class="site-info">
-            Powered By <a href="https://github.com/blush-dev/blush">Blush</a> with Love
-        </div>
-    </footer>
-</section>
-<script rel="javascript" src="<?= assets( 'assets/js/app.js' ) ?>"></script>
-</body>
-</html>
